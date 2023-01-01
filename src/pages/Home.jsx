e19b52627ea07a0c0ba95de8d1f609fcd5dcad29
Rajnish Pratap Singh <rps.rajnish@gmail.com>
2023-01-02 00:43:54 +0530

    Mobile responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Chat from "./components/Chat"
 
 export default function Home() {
   return (
-    <div className="p-8 flex h-screen w-full justify-center items-center ">
+    <div className="p-8 flex h-full w-full justify-center items-center ">
       <div className="flex h-full rounded-xl truncate m-4 w-3/4">
         <Sidebar />
         <Chat />

6df741cba805132a59401f640605317c36f18878
Rajnish Pratap Singh <rps.rajnish@gmail.com>
2023-01-02 00:52:28 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import Sidebar from "./components/Sidebar"
 import Chat from "./components/Chat"
+Import {Link} from "react-router-dom"
 
 export default function Home() {
   return (
+<Link to="/signin">signin</Link>
     <div className="p-8 flex h-full w-full justify-center items-center ">
       <div className="flex h-full rounded-xl truncate m-4 w-3/4">
         <Sidebar />
